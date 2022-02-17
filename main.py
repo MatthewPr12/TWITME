fe8376b68248506a197ac16de79440a5c64006d9
MatthewPr12 <matvii.prytula@ucu.edu.ua>
2022-02-18 01:58:27 +0200

    added form

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from typing import Optional
 from twitter_api import execute
 from geocoder import get_coords
@@ -9,20 +9,26 @@ from fastapi.templating import Jinja2Templates
 app = FastAPI(title='User Friends Locations')
 templates = Jinja2Templates(directory='maps')
 
-@app.get("/")
-async def root():
-    return {'message': 'Hello world!'}
+
+@app.get("/", response_class=HTMLResponse)
+def write_home(request: Request):
+    return templates.TemplateResponse('home.html', {'request': request})
 
 
-@app.get("/about")
-def about():
-    return {'Data': 'Something'}
-
-
-@app.get('/location', response_class=HTMLResponse)
-def location(request: Request, user_name: Optional[str] = None):
+@app.post("/submitform", response_class=HTMLResponse)
+async def get_username(request: Request, user_name: str = Form(...)):
     friends_list = execute(user_name)
     friends = get_coords(friends_list)
     my_map = create_map(friends)
     my_map.save('maps/my_map.html')
     return templates.TemplateResponse('my_map.html', {'request': request})
+    # return location(request, user_name)
+
+
+# @app.get('/location', response_class=HTMLResponse)
+# def location(request: Request, user_name: Optional[str]):
+#     friends_list = execute(user_name)
+#     friends = get_coords(friends_list)
+#     my_map = create_map(friends)
+#     my_map.save('maps/my_map.html')
+#     return templates.TemplateResponse('my_map.html', {'request': request})
