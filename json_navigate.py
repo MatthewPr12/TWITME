4d723ddce0336864720039b7171de2c46240637e
MatthewPr12 <matvii.prytula@ucu.edu.ua>
2022-02-18 01:59:15 +0200

    decorated

--- json_navigate.py
+++ json_navigate.py
@@ -43,10 +43,41 @@ def goto_field(data, key):
     return val
 
 
+def check_way(way: str):
+    way_lst = way.split()
+    if way_lst[0] == 'cd' and len(way_lst) == 2:
+        return True
+
+    return False
+
+
 def main():
-    data = data_loading('twitter1.json')
+    while True:
+        print('\033[96m' + f"Put the name of JSON file you'd like to navigate through" + '\033[0m')
+        file_name = input('>>> ')
+        try:
+            data = data_loading(file_name)
+            break
+        except FileNotFoundError:
+            print('\033[96m' + "The file does not exist. Try again..." + '\033[0m')
+            continue
     display_keys(data)
-    print(goto_field(data, 'users[].location'))
+    print('\033[96m' + 'cd <total path to the key>\n'
+                       'ls  # to see ALL the keys' + '\033[0m')
+    while True:
+        str_jmes = input('>>> ')
+        res_lst = str_jmes.split()
+        if res_lst[0] == 'cd' and len(res_lst) == 2:
+            content = goto_field(data, res_lst[1])
+            js_content = json.dumps(content, indent=4)
+            print(js_content)
+        elif res_lst[0] == 'cd' and len(res_lst) == 1:
+            continue
+        elif str_jmes == 'ls':
+            display_keys(data)
+        else:
+            print('\033[96m' + "command not found" + '\033[0m')
+            continue
 
 
 if __name__ == '__main__':
