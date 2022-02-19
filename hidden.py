9a4205551a490cd01f1b6fca88c005614ceefeb4
MatthewPr12 <matvii.prytula@ucu.edu.ua>
2022-02-19 14:40:09 +0200

    changed requiremets.txt

---
+++ hidden.py
@@ -0,0 +1,10 @@
+# Keep this file separate
+
+# https://apps.twitter.com/
+# Create new App and get the four strings
+
+def oauth():
+    return {"consumer_key": "12BmkbLqJRWbu5IrtKYIZPt73",
+            "consumer_secret": "yyod4BpZ5BLwfbW0DTlBCx5MjSukpC3AU0nFUqW48NvRDz46yI",
+            "token_key": "1174782813113110542-f9An1faV5uid0V15JQBY8Tzgcv5Tbt",
+            "token_secret": "yCQcFQKjJHRYuBm4tUsii0Dn7GxhNTO7oBlU86FvqDIgK"}
